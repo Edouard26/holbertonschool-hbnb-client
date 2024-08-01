d5a202c66260ded51f09ef597c89f3269e869769
Edouard26 <edouard.halimi@gmail.com>
2024-08-01 11:07:32 +0200

    Q.3: Fetch place details from the API using the place ID

---
+++ scripts/index.js
@@ -0,0 +1,51 @@
+/* Homepage JavaScript file */
+
+document.addEventListener('DOMContentLoaded', () => {
+    const login_button = document.getElementById("login-button");
+    const authToken = document.cookie.includes("token=");
+  
+    if (authToken) {
+      login_button.style.display = "none";
+      fetchPlaces(authToken);
+    } else {
+      login_button.style.display = "block";
+    };
+  
+    async function fetchPlaces(token) {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/places', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        });
+  
+        if (response.ok) {
+          const places = await response.json();
+          displayPlaces(places);
+          // populateCountryFilter(places);
+        } else {
+          console.error('Failed to load places:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  
+    function displayPlaces(places) {
+      const placesList = document.getElementById('places-list');
+      placesList.innerHTML = '';
+  
+      places.forEach(place => {
+        const placeElement = document.createElement('div');
+        placeElement.classList.add('place');
+        placeElement.dataset.country = place.country;
+        placeElement.innerHTML =
+          `<h2>${place.name}</h2>
+           <p>${place.description}</p>
+           <p><strong>Location:</strong> ${place.location}</p>`;
+        placesList.appendChild(placeElement);
+      });
+    }
+  });
---
+++ scripts/login.js
@@ -0,0 +1,35 @@
+/* This script is in responsible for login without browser refresh
+   Note: this only works in a server  */
+   document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+    if (loginForm) {
+       loginForm.addEventListener('submit', async (event) => {
+          event.preventDefault();
+ 
+          const email = document.getElementById('email').value;
+          const password = document.getElementById('password').value;
+ 
+          try {
+             const response = await fetch('http://127.0.0.1:5000/login', {
+                method: 'POST',
+                headers: {
+                   'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password })
+             })
+             if (response.ok) {
+                const data = await response.json();
+                document.cookie = `token=${data.token}; path=/; secure; samesite=strict`;
+                window.location.href = 'index.html';
+             } else {
+                // Display an error message
+                document.getElementById('errorMessage').textContent = 'Login failed: ' + (response.statusText || 'Unknown error');
+             };
+ 
+          } catch (error) {
+             console.error('Error:', error);
+             document.getElementById('errorMessage').textContent = 'Login failed: Network error';
+          }
+       });
+    }
+ });
